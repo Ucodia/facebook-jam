ecec0ff741af0930bb687b55bcf5d1b46a40def0
Lionel Ringenbach <ucodia@live.com>
2015-10-30 14:40:21 +0100

    Improved story filtering to avoid repetitions

--- facebook-jam.js
+++ facebook-jam.js
@@ -4,6 +4,8 @@
 	if ($timeline.length == 0)
 		return;
 
+	var jammed = {};
+
 	chrome.runtime.sendMessage({ topic: 'showPageAction' });
 
 	function subscribe() {
@@ -16,18 +18,21 @@
 
 	function updateJam() {
 		var $stories = $timeline.find("div[data-testid='fbfeed_story']").filter(function (i) {
-			var d = $(this).data();
-			return d.hasOwnProperty('timestamp')
+			var $story = $(this);
+			var d = $story.data();
+			return d.hasOwnProperty('timestamp') &&
+						 $story.find("._5mxv").length > 0 &&
+						 jammed.hasOwnProperty(d.dedupekey) === false;
 		});
 
 		unsubscribe();
 
 		$.each($stories, function (i, story) {
 			var $story = $(story);
-			if ($story.find("._5mxv").length > 0) {
-				var $a = $story.find("._3x-2");
-				$a.addClass("facebook-jam-collapsed");
-			}
+			jammed[$story.data().dedupekey] = $story;
+
+			var $content = $story.find("._3x-2").first();
+			$content.addClass("facebook-jam-collapsed");
 		});
 
 		subscribe();
